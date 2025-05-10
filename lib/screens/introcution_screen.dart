56fe8357ac4e2f2151bf5a6bd52f0e053b0c6386
corentin lechêne <corentin.lechene.pro@gmail.com>
2025-05-10 20:20:29 +0200

    feat(introduction): screen finished

--- lib/screens/introcution_screen.dart
+++ lib/screens/introcution_screen.dart
@@ -1,5 +1,8 @@
+import 'package:doctodoc_mobile/shared/widgets/buttons/primary_button.dart';
+import 'package:doctodoc_mobile/shared/widgets/buttons/secondary_button.dart';
 import 'package:flutter/material.dart';
 
+import '../shared/widgets/buttons/base/button_base.dart';
 import '../shared/widgets/modals/login_modal.dart';
 import '../shared/widgets/modals/register_modal.dart';
 
@@ -9,35 +12,79 @@ class IntroductionScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 24.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              const Spacer(),
-              ElevatedButton(
-                onPressed: () => showLoginModal(context, false),
-                child: const Text('Login'),
-              ),
-              const SizedBox(height: 24),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  InkWell(
-                    child: const Text("Don’t have an account? "),
-                    onTap: () => showRegisterModal(context, false),
-                  ),
-                  const Text(
-                    "Sign Up",
-                    style: TextStyle(fontWeight: FontWeight.bold),
+      body: Stack(
+        children: [
+          Positioned.fill(
+            child: Image.asset(
+              'assets/images/doctors.png',
+              fit: BoxFit.cover,
+            ),
+          ),
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: Container(
+              padding: const EdgeInsets.only(right: 24.0, left: 24.0, bottom: 50, top: 24.0),
+              decoration: BoxDecoration(
+                color: Theme.of(context).colorScheme.surface,
+                borderRadius: const BorderRadius.vertical(top: Radius.circular(24)),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withAlpha(77),
+                    blurRadius: 20,
+                    offset: const Offset(0, -10),
                   ),
                 ],
               ),
-              const SizedBox(height: 24),
-            ],
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Image.asset(
+                    'assets/images/doctodoc-logo.png',
+                  ),
+                  const SizedBox(height: 30),
+                  Text(
+                    "Un suivi santé simple, rapide et sécurisé.",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Theme.of(context).colorScheme.onSecondary,
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(height: 30),
+                  PrimaryButton(
+                    label: "Se connecter",
+                    onTap: () {
+                      showLoginModal(context, false);
+                    },
+                  ),
+                  const SizedBox(height: 20),
+                  Row(
+                    children: [
+                      Text(
+                        "Vous n'avez pas de compte ?",
+                        style: TextStyle(
+                          color: Theme.of(context).colorScheme.onSecondary,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                      const SizedBox(width: 8),
+                      Text(
+                        'Inscrivez-vous',
+                        style: TextStyle(
+                          color: Theme.of(context).colorScheme.primary,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
           ),
-        ),
+        ],
       ),
     );
   }
