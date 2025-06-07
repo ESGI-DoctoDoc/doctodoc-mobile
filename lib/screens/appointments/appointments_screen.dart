f1eb5824a9e49151a25c590eb3d567e1f40bc009
corentin lechêne <corentin.lechene.pro@gmail.com>
2025-06-07 18:12:15 +0200

    feat(appointments): add incoming and past appointments screens with loading states

--- lib/screens/appointments/appointments_screen.dart
+++ lib/screens/appointments/appointments_screen.dart
@@ -1,9 +1,7 @@
-import 'package:doctodoc_mobile/blocs/appointment_blocs/display_appointment_bloc/display_appointment_bloc.dart';
-import 'package:doctodoc_mobile/models/appointment.dart';
-import 'package:doctodoc_mobile/screens/appointment/widgets/onboarding_loading.dart';
-import 'package:doctodoc_mobile/shared/widgets/cards/appointment_card.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'appointments_in_coming.dart';
+import 'appointments_past.dart';
 
 class AppointmentsScreen extends StatefulWidget {
   const AppointmentsScreen({super.key});
@@ -14,16 +12,18 @@ class AppointmentsScreen extends StatefulWidget {
 
 class _AppointmentsScreenState extends State<AppointmentsScreen> with TickerProviderStateMixin {
   late TabController _tabController;
-  List<Widget> _pastAppointments = [];
-  bool _isLoadingMore = true;
   final ScrollController _scrollController = ScrollController();
 
   @override
   void initState() {
     super.initState();
     _tabController = TabController(length: 2, vsync: this);
-    _scrollController.addListener(_onScroll);
-    _fetchInitialAppointments();
+    _tabController.addListener(() {
+      if (_tabController.indexIsChanging) {
+        _scrollController.jumpTo(0);
+        setState(() {});
+      }
+    });
   }
 
   @override
@@ -77,13 +77,9 @@ class _AppointmentsScreenState extends State<AppointmentsScreen> with TickerProv
           AnimatedBuilder(
             animation: _tabController,
             builder: (context, child) {
-              return SliverList(
-                delegate: SliverChildListDelegate(
-                  _tabController.index == 0
-                      ? _buildInComingAppointmentsTab()
-                      : _buildPastAppointmentsTab(),
-                ),
-              );
+              return _tabController.index == 0
+                  ? const AppointmentInComing()
+                  : const AppointmentPast();
             },
           ),
         ],
@@ -91,87 +87,6 @@ class _AppointmentsScreenState extends State<AppointmentsScreen> with TickerProv
     );
   }
 
-  List<Widget> _buildInComingAppointmentsTab() {
-    return [
-      BlocBuilder<DisplayAppointmentBloc, DisplayAppointmentState>(
-        builder: (context, state) {
-          return Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: BlocBuilder<DisplayAppointmentBloc, DisplayAppointmentState>(
-              builder: (context, state) {
-                return switch (state.status) {
-                  DisplayAppointmentStatus.initial ||
-                  DisplayAppointmentStatus.initialLoading =>
-                    const OnboardingLoading(), // todo : Corentin is ok ?
-                  DisplayAppointmentStatus.success ||
-                  DisplayAppointmentStatus.loading =>
-                    _buildSuccess(state.appointments, state.isLoadingMore),
-                  DisplayAppointmentStatus.error => _buildError(),
-                };
-              },
-            ),
-          );
-        },
-      )
-    ];
-  }
-
-  Widget _buildSuccess(List<Appointment> appointments, bool isLoadingMore) {
-    List<Widget> appointmentsUpComings = [];
-
-    for (var appointment in appointments) {
-      appointmentsUpComings.add(
-        Padding(
-          padding: const EdgeInsets.symmetric(vertical: 4.0),
-          child: AppointmentCard(
-            appointment: appointment,
-          ),
-        ),
-      );
-    }
-
-    _isLoadingMore = isLoadingMore;
-    Widget lastOne = isLoadingMore ? const CircularProgressIndicator() : const Text('nothing');
-
-    return Column(
-      children: [
-        ...appointmentsUpComings,
-        lastOne,
-      ],
-    );
-  }
-
-  Widget _buildError() {
-    return const Center(
-      child: Text("Une erreur s'est produite."),
-    );
-  }
-
-  List<Widget> _buildPastAppointmentsTab() {
-    return [
-      Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Column(
-          children: [
-            ..._pastAppointments,
-            // if (_isLoadingMore) const CircularProgressIndicator(),
-          ],
-        ),
-      )
-    ];
-  }
-
-  void _onScroll() {
-    if (_scrollController.position.pixels >= _scrollController.position.maxScrollExtent &&
-        _isLoadingMore) {
-      _fetchInitialAppointments();
-    }
-  }
-
-  void _fetchInitialAppointments() {
-    final displayAppointmentBloc = context.read<DisplayAppointmentBloc>();
-    displayAppointmentBloc.add(OnGetAllUpComing());
-  }
 }
 
 class _FixedTextHeader extends SliverPersistentHeaderDelegate {
